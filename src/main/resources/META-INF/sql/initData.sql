c7a8d8db1f5385aa2c27fefa17142d5ef9e8c868
Mariusz Guść <mariuszgusc@gmail.com>
2019-01-27 14:19:53 +0100

    - new initData

--- src/main/resources/META-INF/sql/initData.sql
+++ src/main/resources/META-INF/sql/initData.sql
@@ -1,3 +1,5 @@
+SET SQL_SAFE_UPDATES = 0;
+
 DELETE FROM user;
 ALTER TABLE user AUTO_INCREMENT = 1;
 DELETE FROM person;
@@ -17,14 +19,16 @@ ALTER TABLE language AUTO_INCREMENT = 1;
 DELETE FROM address;
 ALTER TABLE address AUTO_INCREMENT = 1;
 
+SET SQL_SAFE_UPDATES = 1;
+
 INSERT INTO user (login, password) VALUES ('testUser1', 'password');
-INSERT INTO person (user_userId, firstName, lastName, dateOfBirth, gender, phone, email, about, idAddress) VALUES (1, 'Jan', 'Nowak', '1980-01-01', true, 123456789, 'example@email.com', 'lubie placki', 1);
+INSERT INTO person (user_idUser, firstName, lastName, dateOfBirth, gender, phone, email, about, idAddress) VALUES (1, 'Jan', 'Nowak', '1980-01-01', true, 123456789, 'example@email.com', 'lubie placki', 1);
 INSERT INTO experience (idPerson, startYear, endYear, company, location, jobTitle, description) VALUES (1, '2002', '2004', 'Biedronka', 'Poznań', 'Kasjer, sprzedawca', 'Obsługa sklepu');
 INSERT INTO responsibility (idExperience, resp) VALUES (1, 'Wykładanie towaru');
 INSERT INTO responsibility (idExperience, resp) VALUES (1, 'Obsługa kasy');
 INSERT INTO experience (idPerson, startYear, endYear, company, location, jobTitle, description) VALUES (1, '2008', 'Present', 'Dworzec PKP', 'Łódź', 'Babcia klozetowa', 'Pobieranie opłat');
-INSERT INTO responsibility (2, 'Czyszczenie toalet');
-INSERT INTO responsibility (2, 'Mycie podłogi');
+INSERT INTO responsibility (idExperience, resp) VALUES (2, 'Czyszczenie toalet');
+INSERT INTO responsibility (idExperience, resp) VALUES (2, 'Mycie podłogi');
 INSERT INTO education (idPerson, college, direction, startYear, endYear) VALUES (1, 'WSRH', 'Wyższa Szkoła Robienia Hałąsu', '2002', '2003');
 INSERT INTO skill (idPerson, skillName, skillLevel) VALUES (1, 'JAVA', 5);
 INSERT INTO skill (idPerson, skillName, skillLevel) VALUES (1, 'SQL', 1);
@@ -32,4 +36,4 @@ INSERT INTO socialMedia (idPerson, name, link) VALUES (1, 'Facebook', 'www.faceb
 INSERT INTO socialMedia (idPerson, name, link) VALUES (1, 'Instagram', 'www.instagram.com');
 INSERT INTO language (idPerson, name, level) VALUES (1, 'Angielski', 'C1');
 INSERT INTO language (idPerson, name, level) VALUES (1, 'Niemiecki', 'B2');
-INSERT INTO addresss (idPerson_idPerson, street, houseNumber, apartmentNumber, postcode, city) VALUES (1, 'Programistyczna', 1, 666, 00-000, 'Wrocław');
+INSERT INTO address (idPerson_idPerson, street, houseNumber, apartmentNumber, postcode, city) VALUES (1, 'Programistyczna', 1, 666, 00-000, 'Wrocław');
